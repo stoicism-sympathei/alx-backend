1df1dd5ae38b7799a89662b4e51f24c3b60ad393
stoicim-sympatheia <mogesanonymous@gmail.com>
2023-08-29 04:04:51 -0400

    fixed

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,18 +1,11 @@
+import redis from 'redis';
 
-import redis from 'redis/dist/index';
-
-// Create a Redis client
+// this creates a new client
 const client = redis.createClient(); 
 // By default redis.createClient() will use 127.0.0.1 and port 6379
 
-// Listen for the 'connect' event to see whether we successfully connected to the Redis server
-client.on('connect', () => {
-  console.log('Redis client connected to the server');
-  // You can also perform other operations here after a successful connection
-});
+// listen for the connect event to see whether we successfully connected to the redis-server
+client.on('connect', () => console.log('Redis client connected to the server'));
 
-// Listen for the 'error' event to check if we failed to connect to the Redis server
-client.on('error', (err) => {
-  console.error(`Redis client not connected to the server: ${err.message}`);
-});
-
+// listen for the error event tocheck if we failed to connect to the redis-server
+client.on('error', (err) => console.error(`Redis client not connected to the server: ${err.message}`));
